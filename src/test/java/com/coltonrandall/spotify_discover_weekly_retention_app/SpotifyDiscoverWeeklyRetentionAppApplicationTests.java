b187605a280214bcba4046ab06b94376073616f6
Colton Randall <coltonrandall.nz@gmail.com>
2026-04-06 21:27:03 +1200

    fix build pipeline

--- src/test/java/com/coltonrandall/spotify_discover_weekly_retention_app/SpotifyDiscoverWeeklyRetentionAppApplicationTests.java
+++ src/test/java/com/coltonrandall/spotify_discover_weekly_retention_app/SpotifyDiscoverWeeklyRetentionAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.coltonrandall.spotify_discover_weekly_retention_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpotifyDiscoverWeeklyRetentionAppApplicationTests {
 
 	@Test
